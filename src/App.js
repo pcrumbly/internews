9251eb22cb6629bfc2555e1dc3dadd810a40edfa
pcrumbly <prestoncrumbly@gmail.com>
2023-09-10 22:35:54 -0500

    UPDATE App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 // src/App.js
 
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Login from './components/Auth/Login';
 import UserProfile from './components/Auth/UserProfile';
 import Register from './components/Auth/Register';
@@ -11,6 +11,16 @@ import Register from './components/Auth/Register';
 function App() {
   return (
     <Router>
+      <div className="App">
+        <h1><a href="/">InterNews</a></h1>
+        
+        <nav>
+          <Link to="/register">Register </Link>
+          <Link to="/login">Login </Link>
+          <Link to="/profile">Profile </Link>
+          {/* Add other links as needed */}
+        </nav>
+      </div>
       <Routes>
         <Route path="/" element={<h1>Welcome to Internews</h1>} />
         <Route path="/login" element={<Login />} />
