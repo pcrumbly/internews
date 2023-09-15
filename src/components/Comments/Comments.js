de6d721430a262fa08cc12b38842ca8e12c02e74
pcrumbly <prestoncrumbly@gmail.com>
2023-09-15 01:38:51 -0500

    Update before refactor

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,51 +1,53 @@
 import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import { firestore } from '../../services/firebase';
 import { collection, query, where, getDocs } from 'firebase/firestore';
 
-function Comments({ linkUID }) {
-  const [comments, setComments] = useState([]);
+function Comments() {
+    const [comments, setComments] = useState([]);
+    const { linkUID } = useParams();
 
-  useEffect(() => {
-    const fetchComments = async () => {
-      const q = query(
-        collection(firestore, 'comments'),
-        where('linkUID', '==', linkUID)
-      );
+    useEffect(() => {
+      const fetchComments = async () => {
+        const q = query(collection(firestore, 'comments'), where('linkUID', '==', linkUID));
+  
+        try {
+          const snapshot = await getDocs(q);
+          const fetchedComments = snapshot.docs.map(doc => ({
+            id: doc.id,
+            ...doc.data()
+          }));
+          setComments(fetchedComments);
+        } catch (error) {
+          console.error("Error fetching comments:", error);
+        }
+      };
+  
+      fetchComments();
+    }, [linkUID]);
 
-      const snapshot = await getDocs(q);
-
-      const fetchedComments = snapshot.docs.map(doc => ({
-        id: doc.id,
-        ...doc.data()
-      }));
-
-      setComments(fetchedComments);
-    };
-
-    fetchComments();
-  }, [linkUID]);
-
-  const renderComments = (parentId) => {
-    return comments
-      .filter(comment => comment.parentCommentUID === parentId)
-      .map(comment => (
-        <div key={comment.id}>
-          <strong>{comment.createdBy}</strong>:
-          <p>{comment.text}</p>
-          <small>{comment.points} points</small>
-          <div style={{ marginLeft: '20px' }}>
-            {renderComments(comment.id)}
-          </div>
+    return (
+        <div>
+          <h3>Comments:</h3>
+          <table>
+            <tbody>
+              {comments.map(comment => (
+                <React.Fragment key={comment.id}>
+                  <tr>
+                    <td><strong>{comment.createdBy}</strong></td>
+                  </tr>
+                  <tr>
+                    <td>{comment.text}</td>
+                  </tr>
+                  <tr>
+                    <td><small>{comment.points} points</small></td>
+                  </tr>
+                </React.Fragment>
+              ))}
+            </tbody>
+          </table>
         </div>
-      ));
-  };
-
-  return (
-    <div>
-      <h3>Comments:</h3>
-      {renderComments(null)}
-    </div>
-  );
+      );
 }
 
 export default Comments;
